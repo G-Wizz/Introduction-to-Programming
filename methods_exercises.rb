9759dffe458fe76cf47b5099515c202812329025
Greg Schmitt <gwizzprg100@gmail.com>
2016-11-23 05:58:44 -0500

    Methods Exercises

---
+++ methods_exercises.rb
@@ -0,0 +1,48 @@
+# Exercise 1
+
+def greeting(name)
+  puts "How are you " + name
+end
+
+ greeting("Greg")
+
+
+ # Exercise 2
+
+ # 1. 2
+ # 2. nil
+ # 3. "Joe"
+ # 4. "four"
+ # 5. nil
+
+
+ # Exercise 3
+
+ def multiply(a, b)
+  puts a * b
+ end 
+
+multiply(3, 4)
+
+
+# Exercise 4
+
+# Prints nothing - It is returned before it can print
+
+
+# Exercise 5
+
+def scream(words)
+  words = words + "!!!!"
+  # removed the return
+  puts words
+end
+
+scream("Yippeee")
+
+# Prints Yippeee!!!!
+
+
+# Exercise 6
+
+# 'calculate_product' is looking for 2 arguments but only receives 1
